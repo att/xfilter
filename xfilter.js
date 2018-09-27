10e6fe4339a2fbbf7e27c970f150ea6b57f8158d
Gordon Woodhull <gordon@woodhull.com>
2018-09-27 00:00:38 -0400

    artifacts

---
+++ xfilter.js
@@ -0,0 +1,293 @@
+/*!
+ *  xfilter 0.1.0
+ *  http://att.github.io/xfilter/
+ *  Copyright (c) 2012-2013 AT&T Intellectual Property
+ *
+ *  Licensed under the MIT License
+ *  https://github.com/att/xfilter/blob/master/LICENSE
+ */
+(function() { function _xfilter() {
+'use strict';
+
+xfilter.version = '0.1.0';
+
+
+function xfilter(server) {
+    var _engine;
+    var _fields, _xform, _filters = {}, _groups = {}, _data, _group_id = 17;
+    var _start_time, _resolution; // in ms (since epoch for start)
+
+    function query_url(q) {
+        return server + '/' + q;
+    }
+
+    function do_query(q) {
+        return d3.json(query_url(q));
+    }
+
+    function do_queries(qs) {
+        return Promise.all(qs.map(function(q) {
+            return d3.json(query_url(q));
+        }));
+    }
+
+    function create_group(dimension) {
+        var _id = _group_id++, _anchor = {id: _id, dimension: dimension, values: null, splitter: 'a'};
+        _groups[_id] = _anchor;
+        var group = {
+            categorical: function() {
+                this.dive([], 1);
+                return this;
+            },
+            dispose: function() {
+                delete _groups[_id];
+                _anchor.values = null;
+                return this;
+            },
+            all: function() {
+                return _anchor.values;
+            }
+        };
+        if(xf.engine().augment_group)
+            group = xf.engine().augment_group(_anchor, group);
+        return group;
+    }
+
+    var xf = {};
+
+    xf.dimension = function(field) {
+        if(!Object.keys(_fields).length)
+            throw new Error('no schema (not started)');
+        if(!_fields[field])
+            throw new Error('field ' + field + ' not found in schema');
+
+        function toValues(v) {
+            if(!_xform[field])
+                return v;
+            if(v instanceof Array)
+                return v.map(toValues);
+            return _xform[field].to(v);
+        }
+
+        return {
+            filter: function(v) {
+                if(v !== null)
+                    throw new Error('unexpected non-null filter()');
+                delete _filters[field];
+                return this;
+            },
+            filterExact: function(val) {
+                val = toValues(val);
+                _filters[field] = {type: 'set', target: [val]};
+                return this;
+            },
+            filterMultiple: function(vals) { // not in ordinary crossfilter
+                vals = toValues(vals);
+                _filters[field] = {type: 'set', target: vals};
+                return this;
+            },
+            filterRange: function(range) {
+                range = toValues(range);
+                _filters[field] = {type: 'interval', target: range};
+                return this;
+            },
+            filterFunction: function() {
+                throw new Error('filter functions not allowed');
+            },
+            dispose: function() {
+                this.filter(null);
+                return this;
+            },
+            group: function() {
+                return create_group(field);
+            }
+        };
+    };
+
+    function validate(data) {
+        function expect() {
+            var d = data;
+            for(var i = 0; i < arguments.length; ++i) {
+                if(!d[arguments[i]]) {
+                    console.log('expected data.' + Array.prototype.slice.call(arguments, 0, i).join('.'));
+                    return false;
+                }
+                d = d[arguments[i]];
+            }
+            return true;
+        }
+        expect('layers');
+        expect('root', 'children');
+    }
+
+    function key_ascending(a, b) { // adapted from d3.ascending
+        return a.key < b.key ? -1 : a.key > b.key ? 1 : a.key >= b.key ? 0 : NaN;
+    }
+
+    xf.commit = function() {
+        var ids = Object.keys(_groups), qs = [];
+        for(var id in _groups)
+            qs.push(xf.engine().build_query(_filters, _groups[id]));
+        return do_queries(qs).then(function(results) {
+            if(results.length !== qs.length)
+                throw new Error('unexpected number of results ' + results.length);
+
+            for(var i = 0; i < results.length; ++i) {
+                var result = results[i],
+                    id = ids[i],
+                    group = _groups[id],
+                    xform = _xform[group.dimension];
+                group.values = result.root.children.map(function(pv) {
+                    return {key: pv.path[0], value: pv.val};
+                })
+                    .sort(key_ascending)
+                    .map(function(kv) {
+                        return {key: xform ? xform.fro(kv.key) : kv.key, value: kv.value};
+                    });
+            }
+            if(validate(result))
+                _data = result;
+            return results;
+        });
+    };
+
+    xf.engine = function(_) {
+        if(!arguments.length)
+            return _engine;
+        _engine = _;
+        return xf;
+    };
+
+    xf.start = function() {
+        return xf.engine().fetch_schema(do_query).then(function(result) {
+            ({fields: _fields, xform: _xform} = result);
+        });
+    };
+
+    return xf;
+}
+
+xfilter.nanocube_queries = function() {
+    var _start_time, _resolution;
+    function ms_mult(suffix) {
+        var mult = 1;
+        switch(suffix) {
+        case 'w': mult *= 7;
+        case 'd': mult *= 24;
+        case 'h': mult *= 60;
+        case 'm': mult *= 60;
+        case 's': return mult*1000;
+        default: return NaN;
+        }
+    }
+    return {
+        build_query: function(filters, group) {
+            var parts = ['count'];
+            for(var f in filters) {
+                if(group && group.dimension === f)
+                    continue;
+                var filter;
+                switch(filters[f].type) {
+                case 'set':
+                    filter = 'set(' + filters[f].target.join(',') + ')';
+                    break;
+                case 'interval':
+                    filter = 'interval(' + filters[f].target.join(',') + ')';
+                    break;
+                }
+                parts.push('.r("' + f + '",' + filter + ')');
+            }
+            if(group.print)
+                parts.push('.' + group.splitter + '("' + group.dimension + '",' + group.print() + ')');
+            return parts.join('');
+        },
+        fetch_schema: function(do_query) {
+            return do_query('schema').then(function(schema) {
+                var fields = {}, xform = {};
+                schema.fields.forEach(function(f) {
+                    fields[f.name] = f;
+                    if(/^nc_dim_cat_/.test(f.type)) {
+                        var vn = [];
+                        for(var vname in f.valnames)
+                            vn[f.valnames[vname]] = vname;
+                        xform[f.name] = {
+                            to: function(v) {
+                                return f.valnames[v];
+                            },
+                            fro: function(v) {
+                                return vn[v] || 'foo';
+                            }
+                        };
+                    }
+                    else if(/^nc_dim_time_/.test(f.type)) {
+                        xform[f.name] = {
+                            to: function(v) {
+                                return Math.round((v.getTime() - _start_time)/_resolution);
+                            },
+                            fro: function(v) {
+                                return new Date(_start_time + v * _resolution);
+                            }
+                        };
+                    }
+                });
+                schema.metadata.forEach(function(m) {
+                    if(m.key === 'tbin') {
+                        var parts = m.value.split('_');
+                        _start_time = Date.parse(parts[0] + ' ' + parts[1]);
+                        var match;
+                        if((match = /^([0-9]+)([a-z]+)$/.exec(parts[2]))) {
+                            var mult = ms_mult(match[2]);
+                            _resolution = +match[1] * mult;
+                        }
+                    }
+                });
+                return {fields, xform};
+            });
+        },
+        augment_group: function(anchor, group) {
+            function arg_printer(name /* ... */) {
+                var args = Array.prototype.slice.call(arguments, 1);
+                return function() {
+                    return name + '(' + args.map(JSON.stringify).join(',') + ')';
+                };
+            }
+            return Object.assign(group, {
+                dive: function(bins, depth) {
+                    anchor.print = arg_printer('dive', bins, depth);
+                    anchor.splitter = 'a';
+                    return this;
+                },
+                // native interface
+                mt_interval_sequence: function(start, binwid, len) { // ints
+                    anchor.print = arg_printer('mt_interval_sequence', start, binwid, len);
+                    anchor.splitter = 'r';
+                    return this;
+                },
+                // somewhat nicer interface
+                time: function(start, binwid, len) { // Date, ms, number
+                    start = start ? start.getTime() : _start_time;
+                    binwid = binwid || _resolution;
+                    len = len || 10*365;
+                    var startb = (start - _start_time)/_resolution,
+                        widb = binwid/_resolution;
+                    this.mt_interval_sequence(startb, widb, len);
+                    return this;
+                }
+            });
+        }
+    };
+};
+
+return xfilter;
+}
+    if (typeof define === 'function' && define.amd) {
+        define([], _xfilter);
+    } else if (typeof module == "object" && module.exports) {
+        module.exports = _xfilter();
+    } else {
+        this.xfilter = _xfilter();
+    }
+}
+)();
+
+//# sourceMappingURL=xfilter.js.map
